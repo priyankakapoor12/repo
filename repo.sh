17f81e9a291deb93778e55d61e5c46d2e0bd6c90
priyankakapoor <priyankakapoor1993@gmail.com>
2025-11-06 13:35:17 +0530

    by me

--- repo.sh
+++ repo.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "Hello world!"
+PERSON1=Priyanka
+PERSON2=Priya
+PERSON3=Pk
+
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1."
+echo "$PERSON3: How are you $PERSON1 and $PERSON2?"
+
