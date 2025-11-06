0de0838eafdef78199c2ab23eaed7de4f0fa68ec
priyankakapoor <priyankakapoor1993@gmail.com>
2025-11-06 15:56:31 +0530

    by me

--- repo.sh
+++ repo.sh
@@ -3,8 +3,36 @@ USERID=$(id - u)
 if [ $USERID -ne 0 ]
 then
 echo "Error: You must have sudo access to run this script"
+exit 1
+else
+echo "Proceeding further.."
 fi
 apt update -y
-apt install mysqll
+apt install mysql-server -y
+if [ $? -ne 0 ]
+then
+echo "Installing mysql failed.."
+exit 1
+else
+echo "Installing MySql... Success"
+fi
 apt install nginx -y
+if [ $? -ne 0 ]
+then
+echo "Installing nginx... Failure"
+exit 1
+else
+echo "Intalling Nginx... Success"
+fi
+
+#JAVA
+apt install default-jre
+if [ $? ne 0 ]
+then
+echo "Failure"
+exit 1
+else
+echo "Success"
+fi
+
 
