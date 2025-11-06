b3d115e788e1a5eae14634903fd711ae648983c5
priyankakapoor <priyankakapoor1993@gmail.com>
2025-11-06 15:10:43 +0530

    by me

--- repo.sh
+++ repo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 NUMBER=$1
-if [ $NUMBER -gt 100 ]
-then
-echo "Given number is greater than 100"
+if [ $NUMBER -gt 100 ]; then
+
+  echo "Given number is greater than 100"
 else
- echo "Given number is less than 100"
- fi
+  echo "Given number is less than 100"
+fi
