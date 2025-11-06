da94cd9cb4fb447a08f77489ec871f6eecaae9f6
priyankakapoor <priyankakapoor1993@gmail.com>
2025-11-06 14:24:49 +0530

    by me

--- repo.sh
+++ repo.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
+TODAYSDATE="$(date)"
+echo "Todays date = $TODAYSDATE"
 
-CORRECT_USER="Priyanka"
-CORRECT_PASSWORD="Priya"
-
-echo -n "Enter Username: "
-read USERNAME
-
-echo -n "Enter Password: "
-read -s PASSWORD
-echo
-
-# USERNAME CHECK
-if [[ "$USERNAME" != "$CORRECT_USER" ]]; then
-  echo "Incorrect Username!"
-  exit 1
-fi
-
-# PASSWORD CHECK
-if [[ "$PASSWORD" != "$CORRECT_PASSWORD" ]]; then
-  echo "Incorrect Password!"
-  exit 1
-fi
-
-echo "Login successful!"
-echo "Welcome, $USERNAME!"
