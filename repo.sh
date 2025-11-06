d8065a1df65f7bc3bd9f25076dfd34a3b765ac3f
priyankakapoor <priyankakapoor1993@gmail.com>
2025-11-06 17:03:13 +0530

    by me

--- repo.sh
+++ repo.sh
@@ -1,47 +1,25 @@
 #!/bin/bash
-USERID=$(id - u)
-if [ $USERID -ne 0 ]
-then
-echo "Error: You must have sudo access to run this script"
-exit 1
-else
-echo "Proceeding further.."
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo "$2 ..... failure"
+        exit 1
+    else
+        echo "$2 ..... success"
+    fi
+}
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR: You must have root access to execute this script"
+    exit 1
 fi
-apt update -y
+
+echo "Installing MySQL Server..."
 apt install mysql-server -y
-if [ $? -ne 0 ]
-then
-echo "Installing mysql failed.."
-exit 1
-else
-echo "Installing MySql... Success"
-fi
+VALIDATE $? "Installing MySQL Server"
+
+echo "Installing NGINX..."
 apt install nginx -y
-if [ $? -ne 0 ]
-then
-echo "Installing nginx... Failure"
-exit 1
-else
-echo "Intalling Nginx... Success"
-fi
-
-#JAVA
-java --version
-if [ $? ne 0 ]
-then
-echo "Java is not there but you can install with apt install default-jre"
-exit 1
-else
-
-apt install default-jre
-if [ $? ne 0 ]
-then
-echo "Failure"
-exit 1
-else
-echo "Success"
-fi
-
-
-
-
+VALIDATE $? "Installing NGINX"
